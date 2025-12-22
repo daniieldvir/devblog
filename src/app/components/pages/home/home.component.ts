cbca1bb239376c4c62347df6a759792b5d776732
Daniiel Dvir <ddvir@kramerav.com>
2025-12-22 14:05:49 +0200

    Fix routing: replace href with routerLink for GitHub Pages

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { FooterComponent } from '../../frame-components/footer/footer.component';
 import { HeaderComponent } from '../../frame-components/header/header.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent],
+  imports: [HeaderComponent, FooterComponent, RouterLink],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
